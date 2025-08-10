ec683fe133892d136e9a403150653d7a5b1ba41a
roysaumya767-code <roysaumya767@gmail.com>
2025-08-10 10:11:42 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         int a = sc.nextInt();
         System.out.println("Enter second number");
         int b = sc.nextInt();
-        System.out.println("press 1 for sum\npress 2 for diff\npress 3 for product\npress 4 for division\npress5 for power");
+        System.out.println("press 1 for sum\npress 2 for diff\npress 3 for product\npress 4 for division");
         int button = sc.nextInt();
         switch (button) {
             case 1: System.out.println(a+b);
@@ -15,9 +15,9 @@ public class Calculator {
             break;
             case 3: System.out.println(a*b);
             break;
-            case 4: System.out.println(a/b);
-            break;
-            default: System.out.println(a^b);
+            default: System.out.println(a/b);
+            
+            
             
             
         }
@@ -26,3 +26,4 @@ public class Calculator {
 
 
         
+
